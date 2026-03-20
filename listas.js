10e211c74453b507c8bc41bcf4222f83c9032dd4
sandravargas01 <sandra.vargas01unicatolica.edu.co>
2026-03-20 15:36:36 -0500

    mipirmerlista

---
+++ listas.js
@@ -0,0 +1,17 @@
+let lista = [1.1, 2, 3.3, "planeta"]
+
+lista.unshift ("caracol");
+console.log (lista);
+
+
+//lista.push "agrega elemento al final de la lista"
+// lista.unsshift(0); eliminar el ultimo elemento de la lista 
+// lista.pop eliminar el primer elemnto de la lista
+// lista.shift(); imprimir la lista
+
+let nombres = ["sandra","andres", "samara"]
+
+for (let i = 0; i < nombres.length; i++) {
+  console.log("Nombre:", nombres[i]);
+  console.log(nombres["samara"])
+}
